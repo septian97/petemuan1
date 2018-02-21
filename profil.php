75aa9b6c8873e11e166dd436887d4eac9e0203cc
septian97 <septian.rizaldi@yahoo.com>
2018-02-21 18:26:24 +0700

    tambah profil

--- profil.php
+++ profil.php
@@ -1,1 +1,12 @@
-$nama
+<?php
+$nama = "Septian Rizaldi";
+$kelas = "4C";
+$prodi = "Teknik Informatika";
+
+echo "<h1> Tugas Pertemuan 1<h1>"; echo "<br>";
+
+echo $nama; echo "<br>";
+echo $kelas; echo "<br>";
+echo $prodi; echo "<br>";
+
+?>
